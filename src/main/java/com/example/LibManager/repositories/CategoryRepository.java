3c0d0787da12c1eccb632402545e287c63409f32
l3bao84 <l3ducb4008@gmail.com>
2023-05-11 19:04:01 +0700

    le bao first commit

--- src/main/java/com/example/LibManager/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LibManager.repositories;
-
-import com.example.LibManager.models.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, String> {
-}
--- src/main/java/com/example/LibManager/repositories/PlCompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LibManager.repositories;
-
-import com.example.LibManager.models.PlCompany;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlCompanyRepository extends CrudRepository<PlCompany, String> {
-}
