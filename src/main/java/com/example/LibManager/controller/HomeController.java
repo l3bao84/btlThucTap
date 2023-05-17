bdfb7c445c8e1ecb7bdbbeaa6e2301b344ddf598
l3bao84 <l3ducb4008@gmail.com>
2023-05-17 17:35:49 +0700

    Lê Bảo thêm controller

---
+++ src/main/java/com/example/LibManager/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.example.LibManager.controller;
+
+import com.example.LibManager.models.Book;
+import com.example.LibManager.models.BookDTO;
+import com.example.LibManager.repositories.BookRepository;
+import com.example.LibManager.repositories.CategoryRepository;
+import com.example.LibManager.services.VNCharacterUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path = "main")
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final CategoryRepository categoryRepository;
+
+    private final BookRepository bookRepository;
+
+    @GetMapping("")
+    public String homePage(ModelMap modelMap) {
+        modelMap.addAttribute("categories", categoryRepository.findAll());
+        modelMap.addAttribute("books", bookRepository.findAll());
+        modelMap.addAttribute("bookDTO", new BookDTO());
+        return "homePage";
+    }
+
+    @PostMapping("/search")
+    public String search(ModelMap modelMap,@ModelAttribute("bookDTO") BookDTO bookDTO) {
+
+        // to remove accent and space
+        String keyword = VNCharacterUtils.removeSpace(VNCharacterUtils.removeAccent(bookDTO.getBookName().trim())).toUpperCase();
+
+        Iterable<Book> books = bookRepository.findAll();
+        ArrayList<Book> foundBooks = new ArrayList<Book>();
+        for (Book book : books){
+            String rootName = VNCharacterUtils.removeSpace(VNCharacterUtils.removeAccent(book.getBookName()));
+            if(rootName.toUpperCase().contains(keyword)) {
+                foundBooks.add(book);
+            }
+        }
+        if(foundBooks.isEmpty()) {
+            modelMap.addAttribute("found", false);
+            return "searchPage";
+        }
+        modelMap.addAttribute("found", true);
+        modelMap.addAttribute("books", foundBooks);
+        return "searchPage";
+    }
+}
